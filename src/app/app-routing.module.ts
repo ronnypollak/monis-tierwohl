9206ad55e6a27f004601b2e9e3ad5f189810932d
Ronny <ronnymailpollak@gmail.com>
2023-10-22 16:28:44 +0200

    Story page content.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ExperiencesComponent } from './experiences/experiences.component';
 import { ServicesComponent } from './services/services.component';
+import { StoryComponent } from './story/story.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect to `home` route
   { path: 'erfahrungen', component: ExperiencesComponent },
   { path: 'dienstleistungen', component: ServicesComponent },
+  { path: 'geschichte', component: StoryComponent },
 ];
 
 @NgModule({
